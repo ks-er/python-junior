427129b466f0a947d410e1a52c477520406fe03a
ks_e <knershova@gmail.com>
2024-05-24 21:34:57 +0300

    Update implementation.py

--- block_1/flow_control/task_3/implementation.py
+++ block_1/flow_control/task_3/implementation.py
@@ -1,3 +1,7 @@
+from datetime import (
+    datetime
+)
+
 def get_days_count_by_month(month):
     """Возвращает количество дней по месяцу
 
@@ -6,4 +10,29 @@ def get_days_count_by_month(month):
 
     Returns: количество дней
     """
-    raise NotImplementedError
+
+    months = {
+        'январь':31,
+        'февраль': getFebriaryDays(),
+        'март':31,
+        'апрель':30,
+        'май':31,
+        'июнь':30,
+        'июль':31,
+        'август':31,
+        'сентябрь':30,
+        'октябрь':31,
+        'ноябрь':30,
+        'декабрь':31
+    }
+
+    if month in months.keys():
+        return months[month]
+    else:
+        return 0
+
+def getFebriaryDays():
+    year = datetime.now().year
+    if year % 4 == 0: 
+        return 29 
+    else: return 28
