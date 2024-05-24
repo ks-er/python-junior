975883630221712ca1e4d45de52e41c2b27f9902
ks_e <knershova@gmail.com>
2024-05-24 22:47:03 +0300

    Update question.py

--- block_1/scope/task_2/question.py
+++ block_1/scope/task_2/question.py
@@ -2,13 +2,18 @@
 Что будет выведено после выполнения кода? Почему?
 """
 
-
 def transmit_to_space(message):
    
-    def data_transmitter():        
-        print(message)
+    def data_transmitter(): #2 определение функции data_transmitter
+        print(message) #4 выводим message, оно пришло в параметре - выводим Test message
 
-    data_transmitter()
+    data_transmitter() #3 вызов data_transmitter #5 после попадаем сюда, 
+        #но возвращаемого значения у transmit_to_space нет
 
 
-print(transmit_to_space("Test message"))
+print(transmit_to_space("Test message")) #1 вызов transmit_to_space #6 возврат из метода transmit_to_space
+    # со значением None (не было возвращаемого значения) и в итоге выводится на экран None
+
+# в терминале
+# Test message
+# None
