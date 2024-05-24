2dba702b7664dbe44b43924bb606ae182700305e
ks_e <knershova@gmail.com>
2024-05-24 21:53:25 +0300

    Update implementation.py

--- block_1/flow_control/task_4/implementation.py
+++ block_1/flow_control/task_4/implementation.py
@@ -1,3 +1,7 @@
+from datetime import (
+    timedelta
+)
+
 def get_next_date(some_date):
     """Возвращает следующую дату для заданной
 
@@ -6,4 +10,5 @@ def get_next_date(some_date):
 
     Returns: следующая дата
     """
-    raise NotImplementedError
+    
+    return some_date + timedelta(days=1)
