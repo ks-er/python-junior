8f13285d3e929ec9f47cec95992ef901f2881d8c
ks_e <knershova@gmail.com>
2024-05-25 16:19:51 +0300

    Update implementation.py

--- block_1/decorators/task_1/implementation.py
+++ block_1/decorators/task_1/implementation.py
@@ -1,6 +1,27 @@
-def time_execution():
+import time
+
+def time_execution_decorator(function):
     """
     Обертка, печатающая время выполнения функции.
     """
 
-    raise NotImplementedError
+    def wrapper(arg):
+        global start_time
+        if (arg < 0 or type(arg) != int):
+             return "0s"
+
+        result = function(arg)
+        return str(time.time() - start_time) + "s"
+
+    return wrapper
+
+@time_execution_decorator
+def get_factorial(num):
+
+    result = 1
+    for n in range(1, num + 1):
+        result *= n
+    return result
+
+start_time = time.time()
+print(get_factorial(5))
