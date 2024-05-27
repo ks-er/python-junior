635b571a2243e9c5e1caed52c1673f22ef76b4a8
ks_e <knershova@gmail.com>
2024-05-27 16:09:25 +0300

    Update implementation.py

--- block_1/methods/task_2/implementation.py
+++ block_1/methods/task_2/implementation.py
@@ -1,11 +1,29 @@
+from block_1.common import (
+    MyException
+)
+
 class ClassFather:
-    pass
+    _name = 'Father-класс'
+    registered_list = []
+
+    @classmethod
+    def register(cls):
+        if cls._name != 'Father-класс' and issubclass(cls, ClassFather):
+            cls.registered_list.append(cls._name)
+        else:
+            raise MyException
+
+    @classmethod
+    def get_name(cls):
+        if issubclass(cls, ClassFather) and cls._name in cls.registered_list:
+            return cls._name
+        else:
+            raise MyException
+   
 
 
 class User1(ClassFather):
-    _name = ''
-
+    _name = 'User1'
 
 class User2(ClassFather):
-    _name = ''
-
+    _name = 'User2'
