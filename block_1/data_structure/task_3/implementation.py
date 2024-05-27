c60bbeddd964227dfa91bb3f1f123dcafab0203e
ks_e <knershova@gmail.com>
2024-05-27 19:05:38 +0300

    Update implementation.py

--- block_1/data_structure/task_3/implementation.py
+++ block_1/data_structure/task_3/implementation.py
@@ -1,5 +1,8 @@
+import copy
+
 def copy_dict(origin_dict: dict) -> dict:
     """
     Функция возвращает копию словаря.
     """
-    raise NotImplementedError
+    new_dict = copy.deepcopy(origin_dict)
+    return new_dict
