346736e6965b66a4b9fdbd471494c6ba188c3ae4
ks_e <knershova@gmail.com>
2024-05-27 13:49:06 +0300

    Update implementation.py

--- block_1/builtin_functions/task_1/implementation.py
+++ block_1/builtin_functions/task_1/implementation.py
@@ -1,2 +1,40 @@
+from block_1.common import (
+    MyException
+)
+
 class Value:
-    pass
+    def __init__(self, value):
+        self.value = value
+
+    def __add__(self, other):
+        try:
+            result = self.value + other
+        except:
+            raise MyException
+        else:
+            return result
+         
+
+    def __sub__(self, other):
+        try:
+            result = self.value - other
+        except:
+            raise MyException
+        else:
+            return result
+    
+    def __mul__(self, other):
+        try:
+            result = self.value * other
+        except:
+            raise MyException
+        else:
+            return result
+    
+    def __truediv__(self, other):
+        try:
+            result = self.value / other        
+        except:
+            raise MyException
+        else:
+            return result
