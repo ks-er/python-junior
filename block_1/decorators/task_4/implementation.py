9d442f4077f643e1359e49d0965e33c952c71743
ks_e <knershova@gmail.com>
2024-05-27 13:25:35 +0300

    Update implementation.py

--- block_1/decorators/task_4/implementation.py
+++ block_1/decorators/task_4/implementation.py
@@ -1,4 +1,10 @@
-def decorator_maker():
+import time
+
+from block_1.common import (
+    MyException,
+)
+
+def decorator_maker(times, delay):
     """
     Обертка, которая повторяет вызов функции times раз с паузой delay секунд
     Args:
@@ -8,5 +14,21 @@ def decorator_maker():
     Returns:
         валидное значение (при вызове bool() -> True)
     """
-    raise NotImplementedError
+    def decorator_fn(function):        
+        def wrapper(*args, **kwargs):
+            for i in range (times):
+                try:
+                    result = function(*args, **kwargs)
+                    if (validate_result(result)):
+                        return result
+                    else:
+                        time.sleep(delay)
+                except:
+                    time.sleep(delay)
 
+            raise MyException
+        return wrapper
+    return decorator_fn
+
+def validate_result(result):
+    return True
