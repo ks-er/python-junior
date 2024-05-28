8f7c42e4aaa41effefba634d7314322365a3aaec
ks_e <knershova@gmail.com>
2024-05-28 20:47:34 +0300

    Update implementation.py

--- block_1/magic_method/task_2/implementation.py
+++ block_1/magic_method/task_2/implementation.py
@@ -1,8 +1,32 @@
+from datetime import time, timedelta
+
 class MathClock:
     def __init__(self):
-        pass
+        self.time = time(00, 00)
+    
+    def __add__(self, min):
+        td =  timedelta(hours=self.time.hour, minutes=self.time.minute)
+        alltd = td + timedelta(minutes=min)
+        sec = alltd.total_seconds()
+        self.time = time(int(alltd.seconds//3600), int((sec-sec//3600*3600)/60))
+
+    def __sub__(self, min):
+        td =  timedelta(hours=self.time.hour, minutes=self.time.minute)
+        alltd = td - timedelta(minutes=min)
+        sec = alltd.total_seconds()
+        self.time = time(int(alltd.seconds//3600), int((sec-sec//3600*3600)/60))
+
+    def __mul__(self, h):
+        td =  timedelta(hours=self.time.hour, minutes=self.time.minute)
+        alltd = td + timedelta(hours=h)
+        sec = alltd.total_seconds()
+        self.time = time(int(alltd.seconds//3600), int((sec-sec//3600*3600)/60))
+    
+    def __truediv__(self, h):
+        td =  timedelta(hours=self.time.hour, minutes=self.time.minute)
+        alltd = td - timedelta(hours=h)
+        sec = alltd.total_seconds()
+        self.time = time(int(alltd.seconds//3600), int((sec-sec//3600*3600)/60))
 
     def get_time(self):
-        return 0
-
-
+        return self.time.strftime("%H:%M")
