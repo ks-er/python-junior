24b550dc9e0442c6479b49e463e2eb616469eecb
ks_e <knershova@gmail.com>
2024-05-28 19:19:29 +0300

    Update implementation.py

--- block_1/magic_method/task_1/implementation.py
+++ block_1/magic_method/task_1/implementation.py
@@ -1,24 +1,94 @@
+import math
+
+from common import (
+    MyException
+)
+
 class Multiplier:
-    def __init__(self):
-        pass
+    def __init__(self, value):
+        self.value = value
+
+    def __add__(self, obj):
+        try:
+            result = Multiplier(self.value + obj.value)
+        except:
+            raise MyException
+        else:
+            return result
+    
+    def __iadd__(self, obj):
+        try:
+            result = Multiplier(self.value + obj.value)
+        except:
+            raise MyException
+        else:
+            return result
+
+    def __sub__(self, obj):
+        try:
+            result = Multiplier(self.value - obj.value)
+        except:
+            raise MyException
+        else:
+            return result
+        
+    def __isub__(self, obj):
+        try:
+            result = Multiplier(self.value - obj.value)
+        except:
+            raise MyException
+        else:
+            return result
+    
+    def __mul__(self, obj):
+        try:
+            result = Multiplier(self.value * obj.value)
+        except:
+            raise MyException
+        else:
+            return result
+    
+    def __imul__(self, obj):
+        try:
+            result = Multiplier(self.value * obj.value)
+        except:
+            raise MyException
+        else:
+            return result
+    
+    def __truediv__(self, obj):
+        try:
+            result = Multiplier(self.value / obj.value)
+        except:
+            raise MyException
+        else:
+            return result
+    
+    def __itruediv__(self, obj):
+        try:
+            result = Multiplier(self.value / obj.value)
+        except:
+            raise MyException
+        else:
+            return result
+
+    def get_value(self):
+        return math.trunc(self.value)
 
 
 class Hundred(Multiplier):
     """Множитель на 100"""
-    def __init__(self):
-        pass
+    def __init__(self, value):
+        self.value = value * 100
 
 
 class Thousand(Multiplier):
     """Множитель на 1 000"""
-    def __init__(self):
-        pass
+    def __init__(self, value):
+        self.value = value * 1000
 
 
 class Million(Multiplier):
     """Множитель на 1 000 000"""
-    def __init__(self):
-        pass
-
-
-
+    def __init__(self, value):
+        self.value = value * 1000000
