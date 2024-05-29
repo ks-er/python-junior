05e9a58ba37199f4d1bcd2d2b457b86b6660263a
ks_e <knershova@gmail.com>
2024-05-29 20:31:49 +0300

    Add files via upload

---
+++ block_1/context_manager/implementation.py
@@ -0,0 +1,17 @@
+class File():
+    def __init__(self, filename, mode):
+        self.filename = filename
+        self.mode = mode
+        self.file = None
+ 
+    def __enter__(self):
+        self.file = open(self.filename, self.mode)
+        print(len(self.file.readlines()))
+        return self.file
+ 
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        self.file.close()
+
+with File('I:/WORK/LEARN CENTER/Python/python-junior/block_1/context_manager/log.txt','r') as file:
+    print("close file")
+    file.close()
