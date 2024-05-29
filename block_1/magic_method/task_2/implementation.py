5a1710584818c2d41377b6e58b05d845e6b6f44a
ks_e <knershova@gmail.com>
2024-05-29 19:31:24 +0300

    Update implementation.py

--- block_1/magic_method/task_2/implementation.py
+++ block_1/magic_method/task_2/implementation.py
@@ -3,30 +3,28 @@ from datetime import time, timedelta
 class MathClock:
     def __init__(self):
         self.time = time(00, 00)
-    
-    def __add__(self, min):
-        td =  timedelta(hours=self.time.hour, minutes=self.time.minute)
-        alltd = td + timedelta(minutes=min)
-        sec = alltd.total_seconds()
-        self.time = time(int(alltd.seconds//3600), int((sec-sec//3600*3600)/60))
+ 
+    def __add__(self, min):        
+        self.time = set_result_time(self.time, timedelta(minutes=min), True)
 
-    def __sub__(self, min):
-        td =  timedelta(hours=self.time.hour, minutes=self.time.minute)
-        alltd = td - timedelta(minutes=min)
-        sec = alltd.total_seconds()
-        self.time = time(int(alltd.seconds//3600), int((sec-sec//3600*3600)/60))
+    def __sub__(self, min):       
+        self.time = set_result_time(self.time, timedelta(minutes=min), False)
 
     def __mul__(self, h):
-        td =  timedelta(hours=self.time.hour, minutes=self.time.minute)
-        alltd = td + timedelta(hours=h)
-        sec = alltd.total_seconds()
-        self.time = time(int(alltd.seconds//3600), int((sec-sec//3600*3600)/60))
+        self.time = set_result_time(self.time, timedelta(hours=h), True)
     
     def __truediv__(self, h):
-        td =  timedelta(hours=self.time.hour, minutes=self.time.minute)
-        alltd = td - timedelta(hours=h)
-        sec = alltd.total_seconds()
-        self.time = time(int(alltd.seconds//3600), int((sec-sec//3600*3600)/60))
+        self.time = set_result_time(self.time, timedelta(hours=h), False)
 
     def get_time(self):
         return self.time.strftime("%H:%M")
+
+
+def set_result_time(self_time, timedel, is_sum):
+    all_td =  timedelta(hours=self_time.hour, minutes=self_time.minute)
+    if is_sum:
+        all_td = all_td + timedel
+    else:
+        all_td = all_td - timedel
+    sec = all_td.total_seconds()
+    return time(int(all_td.seconds//3600), int((sec-sec//3600*3600)/60))
