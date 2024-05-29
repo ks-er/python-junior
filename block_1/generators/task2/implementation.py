83fdf53cfc666ebf8c00e2fe8e30ee0a22bfa411
ks_e <knershova@gmail.com>
2024-05-29 20:07:21 +0300

    Update implementation.py

--- block_1/generators/task2/implementation.py
+++ block_1/generators/task2/implementation.py
@@ -2,6 +2,6 @@ def file_reader(file_name):
     for row in open(file_name, "r"):
         yield row
 
-for rr in file_reader('I:/WORK/LEARN CENTER/Python/python-junior/block_1/generators/task2/log.txt'):
+for rr in file_reader('log.txt'):
     if rr.find("ERROR") != -1:
         print(rr)
