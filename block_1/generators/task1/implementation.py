4e0b0000dbbd504a35b9a4782518884445a0af3f
ks_e <knershova@gmail.com>
2024-05-29 19:47:31 +0300

    Update implementation.py

--- block_1/generators/task1/implementation.py
+++ block_1/generators/task1/implementation.py
@@ -1,2 +1,9 @@
-def fib():
-    raise NotImplementedError
+def fib(number):
+    num1 = 1
+    yield num1
+    num2 = 1
+    yield num2
+
+    for i in range(number - 2):
+        num1, num2 = num2, num1 + num2
+        yield num2
