868ff34cfddee0360c02fe23bdc15c32ca8adffc
ks_e <knershova@gmail.com>
2024-06-16 16:15:26 +0300

    Update implementation.py

--- block_2/queryset_methods/task_2/implementation.py
+++ block_2/queryset_methods/task_2/implementation.py
@@ -7,4 +7,16 @@ def get_top_customer_in_period(begin, end):
 
     Returns: возвращает имя покупателя и количество его заказов за указанный период
     """
-    raise NotImplementedError
+    q_start_date = Q(order__date_formation__gte=begin)
+    q_end_date = Q(order__date_formation__lte=end)
+
+    max_order = (Customer.objects.annotate(
+        period_orders=Count('order', filter=q_start_date & q_end_date)
+        ).order_by('-period_orders'))[:1]
+
+    res = max_order.values_list('name', 'period_orders')
+
+    if res[0][1] > 0:
+        return res[0]
+    else:
+        return None
