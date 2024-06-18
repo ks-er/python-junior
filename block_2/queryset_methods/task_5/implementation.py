c12a8e32fc4cb50413b87cbbfb372d62754c9db5
ks_e <knershova@gmail.com>
2024-06-18 08:41:51 +0300

    Update implementation.py

--- block_2/queryset_methods/task_5/implementation.py
+++ block_2/queryset_methods/task_5/implementation.py
@@ -15,7 +15,16 @@ def get_average_cost_without_product(product, begin, end):
     f_end_date = Q(order__date_formation__lte=end)
     f_product = Q(product__name=product)
 
-    order_item_query = OrderItem.objects.filter(f_start_date & f_end_date & f_product)
+    order_item_product_query = (OrderItem.objects.filter(f_start_date & f_end_date & f_product)
+                                .values('order_id').distinct())
+
+    order_id_list = list()
+    for row in order_item_product_query:
+        order_id_list.append(
+            row['order_id']
+        )
+
+    order_item_query = OrderItem.objects.filter(f_start_date & f_end_date).exclude(order_id__in=order_id_list)
 
     f1 = Q(begin__lte=OuterRef("order__date_formation"))
     f2 = Q(end__gte=OuterRef("order__date_formation"))
