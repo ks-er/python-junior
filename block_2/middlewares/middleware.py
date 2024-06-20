2edf0723e513dee3c7c3ad67ec78d7d67da32c15
ks_e <knershova@gmail.com>
2024-06-20 12:52:42 +0300

    Update middleware.py

--- block_2/middlewares/middleware.py
+++ block_2/middlewares/middleware.py
@@ -34,13 +34,18 @@ class FormatterMiddleware(MiddlewareMixin):
     {'key': value} => <p>key = value</p>
     """
     def process_response(self, request, response):
-        data = json.loads(response.content.decode())
+        try:
+            decode_content = response.content.decode()
+            data = json.loads(decode_content)
 
-        new_data = list()
-        for item in data.items():
-            new_data.append(f'<p>{item[0]} = {item[1]}</p>')
+            new_data = list()
+            for item in data.items():
+                new_data.append(f'<p>{item[0]} = {item[1]}</p>')
 
-        return HttpResponse(new_data)
+            return HttpResponse(new_data)
+
+        except:
+            return response
 
 
 class CheckErrorMiddleware(MiddlewareMixin):
@@ -49,7 +54,6 @@ class CheckErrorMiddleware(MiddlewareMixin):
         "Ошибка: {exception}"
     """
     def process_exception(self, request, exception):
-        print('была ошибка')
         return HttpResponse(f"Ошибка: {exception}")
 
 
