1651b1bd344b0d934c7465ea06a05ddef70a1800
ks_e <knershova@gmail.com>
2024-06-20 08:24:19 +0300

    Update views.py

--- block_2/middlewares/views.py
+++ block_2/middlewares/views.py
@@ -21,3 +21,46 @@ def calc(request):
         return JsonResponse(list)
     else:
         pass
+
+class Calculator():
+
+    @classmethod
+    def calculate(cls, maths, delimiter):
+        list = maths.split(delimiter)
+
+        dict = {}
+        for item in list:
+            if ('+' in item):
+                dict[item] = getItemValue(item, '+')
+            elif ('-' in item):
+                dict[item] = getItemValue(item, '-')
+            elif ('*' in item):
+                dict[item] = getItemValue(item, '*')
+            elif ('/' in item):
+                dict[item] = getItemValue(item, '/')
+            else:
+                dict[item] = item
+
+        return dict
+
+def getValues(str, delimiter):
+    return str.split(delimiter)
+
+def getItemValue(item, delimiter):
+    values = getValues(item, delimiter)
+    if (len(values) != 2):
+        return item
+
+    v1 = int(values[0])
+    v2 = int(values[1])
+
+    if (delimiter == '+'):
+        return v1 + v2
+    elif (delimiter == '-'):
+        return v1 - v2
+    elif ('*' in item):
+        return v1 * v2
+    elif ('/' in item):
+        return v1 / v2
+    else:
+        return item
