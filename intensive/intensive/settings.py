607b46ac57d1d6d7d9564d447dc240fb7ce9798e
ks_e <knershova@gmail.com>
2024-06-20 08:48:53 +0300

    Update settings.py

--- intensive/intensive/settings.py
+++ intensive/intensive/settings.py
@@ -47,8 +47,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'middlewares.middleware.StatisticMiddleware'
-    'middlewares.middleware.CheckErrorMiddleware'
+    'middlewares.middleware.StatisticMiddleware',
+    'middlewares.middleware.CheckErrorMiddleware',
     'middlewares.middleware.FormatterMiddleware'
 ]
 
